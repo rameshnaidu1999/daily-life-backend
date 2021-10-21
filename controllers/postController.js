775ec3af05ea20f3c4d8e4e593d15e2af8c74c9c
ramesh.k <ramesh.k@technoduce.com>
2021-10-21 16:23:57 +0530

    delete update methods added

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,15 +3,39 @@ import PostsModel from "../models/postModel.js";
 const getAllPosts = async (req, res) => {
   await PostsModel.find((err, data) => {
     if (err) {
-      res.status(500).send(err);
+      res.status(500).json({
+        msg: "failed",
+        status: 500,
+        error: err.message,
+      });
     } else {
-      res.status(200).send({
+      res.status(200).json({
         status: 200,
         message: "Posts",
         data: data,
       });
     }
   });
+};
+
+const getPostById = async (req, res) => {
+  const id = req.params.id;
+  console.log(req.params.id);
+  await PostsModel.findById(req.params.id)
+    .then((result) => {
+      res.status(200).json({
+        status: 200,
+        msg: "fetched",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        msg: "failed",
+        status: 500,
+        error: err.message,
+      });
+    });
 };
 
 const createPost = async (req, res) => {
@@ -29,4 +53,45 @@ const createPost = async (req, res) => {
   });
 };
 
-export { getAllPosts, createPost };
+const deletePost = async (req, res, next) => {
+  console.log(req.params.id);
+  await PostsModel.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(200).json({
+        data: result,
+        msg: "deleted",
+        status: 200,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        msg: "failed",
+        status: 500,
+        error: err.message,
+      });
+    });
+};
+
+const updateById = async (req, res) => {
+  const { title, postImageUrl } = req.body;
+  await PostsModel.findByIdAndUpdate(req.params.id, {
+    title,
+    postImageUrl,
+  })
+    .then((result) => {
+      res.status(200).json({
+        data: result,
+        msg: "updated",
+        status: 200,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        msg: "failed",
+        status: 500,
+        error: err.message,
+      });
+    });
+};
+
+export { getAllPosts, createPost, getPostById, deletePost, updateById };
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { createPost, getAllPosts } from "../controllers/postController.js";
+import {
+  createPost,
+  deletePost,
+  getAllPosts,
+  getPostById,
+  updateById,
+} from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.get("/getall", getAllPosts);
 
 router.post("/create", createPost);
+
+router.get("/get/:id", getPostById);
+
+router.delete("/delete/:id", deletePost);
+
+router.put("/update/:id", updateById);
 
 export default router;
